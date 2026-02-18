d35ea693108d497ef6ae6db0d007a6f7818f8ad9
XiaoHui406 <1052120423@qq.com>
2026-02-18 23:03:58 +0800

    feat: 为选择不同的ASRModelManager定义了适配器

---
+++ backend/manager/asr/asr_model_adapter.py
@@ -0,0 +1,32 @@
+from typing import List, Dict
+from model.asr_model_config import ASRModelConfig, ASRModelJsonConfig
+from manager.asr.interface.asr_model_manager import ASRModelManager
+from manager.asr.whisper_model_manager import WhisperModelManager
+from manager.asr.funasr_model_manager import FunasrModelManager
+import json
+
+
+class ASRModelAdapter:
+    def __init__(self) -> None:
+        self.asr_model_config_map: Dict[str, ASRModelConfig] = {}
+        with open('config/asr_model.json', 'r', encoding='utf8') as config_file:
+            data: ASRModelJsonConfig = ASRModelJsonConfig(
+                **json.loads(config_file.read())
+            )
+        for model in data.models:
+            self.asr_model_config_map[model.name] = model
+
+    def get_asr_model_manager(
+        self,
+        model_name: str,
+        model_size: str,
+        device: str
+    ) -> ASRModelManager:
+        asr_model_config: ASRModelConfig = self.asr_model_config_map[model_name]
+        match asr_model_config.type:
+            case 'whisper':
+                return WhisperModelManager(model_size=model_name, device=device)
+            case 'funasr':
+                return FunasrModelManager(model_size=model_size, device=device)
+            case _:
+                raise ValueError(f'model {model_name} is not supported')
